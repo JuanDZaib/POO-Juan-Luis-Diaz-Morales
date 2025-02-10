c9928a97fb53164e3ee76ecf0d50bd591f18d0e7
JuanDZaib <juan.diazm@axity.com>
2025-02-10 10:14:26 -0600

    Primer ejercicio

---
+++ ejercicio_1.ts
@@ -0,0 +1,25 @@
+class Car{
+    protected nombre: string;
+    protected sexo:string;
+    protected edad:number;
+    protected peso:number;
+    protected color:string;
+    protected textura:string;
+
+    constructor(nombre:string,sexo:string,peso:number,edad:number,color:string,textura:string){
+    this.nombre=nombre;
+    this.sexo=sexo;
+    this.edad=edad;
+    this.peso=peso;
+    this.color=color;
+    this.textura=textura;
+    }
+    info(): void{
+        console.log(`Nombre del gatito: ${this.nombre}\n sexo: ${this.sexo}\n edad: ${this.edad} años \n peso: ${this.peso} Kg\n color: ${this.color} \n textura: ${this.textura}`)
+    } 
+}
+let oscar = new Car('Oscar','Macho', 2, 3.5, 'Negro','Peludito');
+let luna = new Car('Luna','Hembra', 3, 2,'Calico', 'Esponjosa');
+
+luna.info();
+oscar.info();
