5a6d9326292677ac7e65bca958b3cb4280e4e38a
JuanDZaib <juan.diazm@axity.com>
2025-02-11 09:51:22 -0600

    Evaluacion inicio

---
+++ src/evaluacion.ts
@@ -0,0 +1,8 @@
+/*Evaluacion POO.
+1. Genera una rama en tu repositorio que compartiste anteriormente con el nombre de evaluacion_poo.
+2. Desarrollar un sistema de gestión de inventario para una papelería que permita realizar operaciones de venta, consultar el inventario y gestionar productos.
+3. Debes de tener almenos 3 productos.
+4. Se debe de guardar en memoria para poder hacer operaciones de aumento, resta, consulta en el numero de productos.
+4. Procura que tu codigo tenga calidad.
+5. Coloca como forma de comentarios en tus clases el por que las creaste.*/
+
